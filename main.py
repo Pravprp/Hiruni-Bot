4bd22f8795273ba754dbe9241f7218693701429c
pravprp <swayamprakash85649@gmail.com>
2026-05-20 13:25:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ safety_settings = [
 ]
 
 model = genai.GenerativeModel(
-    model_name='gemini-3.1-pro', 
+    model_name='gemini-3.5-flash', 
     system_instruction=(
         "Your name is Hiruni. You are a Sri Lankan girl.\n\n"
         "Behaviour:\n"
